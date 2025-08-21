d1ee67becc030d8e220d48afcfd6e3dd70711e74
Tim Aerts <aardbeiplantje@gmail.com>
2025-08-21 12:46:19 +0200

    Refactor at_setup.sh to streamline command execution by replacing do_uart function with a heredoc for better readability and maintainability

--- at_setup.sh
+++ at_setup.sh
@@ -11,17 +11,14 @@ SSID=${2:-${SSID?$(usage "Error: SSID is not specified.")}}
 PASS=${3:-${PASS?$(usage "Error: Password is not specified.")}}
 R_HOST=${4:-${R_HOST?$(usage "Error: Remote host is not specified.")}}
 R_PORT=${5:-5775}
-{
-do_uart(){
-    local cmd="$1"
-    echo -ne "$cmd\n"
-}
-do_uart "AT+VERBOSE=0"
-do_uart "AT+IPV4=dhcp"
-do_uart "AT+IPV6=dhcp"
-do_uart "AT+WIFI_SSID=$SSID"
-do_uart "AT+WIFI_PASS=$PASS"
-do_uart "AT+UDP_PORT=$R_PORT"
-do_uart "AT+UDP_HOST_IP=$R_HOST"
-do_uart "AT+VERBOSE=0"
-}| perl ble_uart.pl ${BLE_ADDR}
+
+cat <<EObleuart | perl ble_uart.pl ${BLE_ADDR}
+AT+VERBOSE=0
+AT+IPV4=dhcp
+AT+IPV6=dhcp
+AT+WIFI_SSID=$SSID
+AT+WIFI_PASS=$PASS
+AT+UDP_PORT=$R_PORT
+AT+UDP_HOST_IP=$R_HOST
+AT+VERBOSE=0
+EObleuart
