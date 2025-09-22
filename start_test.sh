7e7912f73dab4c4521b8ff2e04e2ed3be1d5c2cf
Tim Aerts <aardbeiplantje@gmail.com>
2025-09-22 21:03:28 +0200

    start_test.sh: more sleepy, add tests

--- start_test.sh
+++ start_test.sh
@@ -2,13 +2,32 @@
 
 uid=$(id -u)
 socat_opt=
+tgt_ipv6="[fe80::eeda:3bff:febf:9400%wlan0]"
+tgt_ipv4="192.168.1.80"
+
+# ipv6 TCP IN+OUT
+tcp_port=56765
+uart_fn=/run/user/$uid/uart_${tcp_port}_tcp_ipv6
+socat -b8 ${socat_opt} pty,link=${uart_fn},raw,unlink-close=0 TCP6-LISTEN:${tcp_port},ipv6only=1,reuseaddr,fork &
+sleep 5
+stty -F ${uart_fn} 115200 cs8 -parenb -cstopb raw -echo
+cat ${uart_fn} &
+echo "$uart_fn"
+
+# ipv4 TCP IN+OUT
+tcp_port=56765
+uart_fn=/run/user/$uid/uart_${tcp_port}_tcp_ipv4
+socat -b8 ${socat_opt} pty,link=${uart_fn},raw,unlink-close=0 TCP4-LISTEN:${tcp_port},reuseaddr,fork &
+sleep 5
+stty -F ${uart_fn} 115200 cs8 -parenb -cstopb raw -echo
+cat ${uart_fn} &
+echo "$uart_fn"
 
 # ipv6 UDP IN/OUT
 udp_port=56765
-tgt_ipv6="[fe80::eeda:3bff:febf:9400%wlan0]"
 uart_fn=/run/user/$uid/uart_${udp_port}_udp_ipv6
 socat -b8 ${socat_opt} UDP6:${tgt_ipv6}:${udp_port},ipv6only=1,reuseaddr pty,link=${uart_fn},raw,unlink-close=0 &
-sleep 2
+sleep 5
 stty -F ${uart_fn} 115200 cs8 -parenb -cstopb raw -echo
 cat ${uart_fn} &
 echo "$uart_fn"
@@ -17,7 +36,7 @@ echo "$uart_fn"
 tgt_ipv4=192.168.1.80
 uart_fn=/run/user/$uid/uart_${udp_port}_udp_ipv4
 socat -b8 ${socat_opt} UDP4:${tgt_ipv4}:${udp_port} pty,link=${uart_fn},raw,unlink-close=0 &
-sleep 2
+sleep 5
 stty -F ${uart_fn} 115200 cs8 -parenb -cstopb raw -echo
 cat ${uart_fn} &
 echo "$uart_fn"
